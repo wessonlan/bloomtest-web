7d242dde5c389066616396c4ce8e3a7bb042ed84
wessonlan <767760325@qq.com>
2022-03-25 11:36:27 +0800

    组件拆分-选择项目

---
+++ src/views/apiManagement/definition/ApiProjectFilter.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-select
+      v-model="projectValue"
+      filterable
+      placeholder="项目"
+      style="width: 120px"
+      value=""
+      @change="queryModuleList()"
+    >
+      <el-option
+        v-for="item in projectList"
+        :key="item.id"
+        :label="item.projectName"
+        :value="item.id"
+      />
+    </el-select>
+  </div>
+</template>
+
+<script>
+import { getModuleList } from '@/api/apiModule'
+
+export default {
+  name: 'ApiProjectFilter',
+  props: {
+    projectList: {
+      type: Array
+    }
+  },
+  data() {
+    return {
+      projectValue: '',
+      moduleListData: []
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+    queryModuleList() {
+      getModuleList(this.projectValue).then(response => {
+        this.moduleListData = response.data
+        // this.currentModuleList = response.data
+      })
+    }
+    // initProjectApi() {
+    //   this.listLoading = true
+    //   getApiListByModuleId(this.value, 0, this.currentPage, this.size)
+    //     .then(response => {
+    //       this.tableData = response.data.records
+    //       this.currentPage = response.data.current
+    //       this.size = response.data.size
+    //       this.total = response.data.total
+    //       this.listLoading = false
+    //     })
+    // }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
