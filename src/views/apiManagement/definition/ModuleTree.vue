dade5e05abd6ba3b5f446f4170fee3c0e5786900
wessonlan <767760325@qq.com>
2022-04-02 18:28:06 +0800

    组件拆分-模块树增删改完成，去掉了对话框组件（避免过度拆分）

--- src/views/apiManagement/definition/ModuleTree.vue
+++ src/views/apiManagement/definition/ModuleTree.vue
@@ -43,23 +43,31 @@
         </span>
       </el-tree>
     </div>
-    <!--模块树编辑对话框组件-->
-    <TreeNodeDialog
-      :module-dialog-state="moduleDialogState"
-    />
+    <!--模块树编辑对话框-->
+    <el-dialog title="节点信息" :visible.sync="dialogFormVisible" destroy-on-close>
+      <el-form :model="form">
+        <el-form-item label="模块名称" :label-width="formLabelWidth" style="width: 50%">
+          <el-input v-model="form.nodeName" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="dialogStatus==='create'? handleNodeAdd():handleNodeUpdate()"
+        > 保 存</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import TreeNodeDialog from '@/views/apiManagement/definition/TreeNodeDialog'
 
 import { getApiListByModuleId } from '@/api/apiDefinition'
-import { deleteNode } from '@/api/apiModule'
+import { getModuleList, deleteNode, addModuleNode, renameModuleNode } from '@/api/apiModule'
 
 export default {
   name: 'ModuleTree',
-  // 注册组件
-  components: { TreeNodeDialog },
   data() {
     return {
       filterText: '',
@@ -69,10 +77,12 @@ export default {
         label: 'label'
       },
       currentNode: {},
-      moduleDialogState: {
-        dialogFormVisible: false,
-        dialogStatus: ''
-      }
+      dialogFormVisible: false,
+      dialogStatus: '',
+      form: {
+        nodeName: ''
+      },
+      formLabelWidth: '80px'
     }
   },
   mounted() {
@@ -91,23 +101,17 @@ export default {
       return data.label.indexOf(value) !== -1
     },
     append(data) {
-      this.moduleDialogState.dialogFormVisible = true
+      this.dialogFormVisible = true
       // 把对话框的值赋给 data的 name字段传给后端
       this.currentNode = data
       // console.log('传入的 node：' + JSON.stringify(data))
-      this.moduleDialogState.dialogStatus = 'create'
-      // console.log('@@!!!', this.moduleDialogState)
+      this.dialogStatus = 'create'
     },
     edit(data) {
-      // console.log(data)
-      // 打开对话框
-      this.moduleDialogState.dialogFormVisible = true
-      // 把 data 赋给 currentNode，用于后面其他方法的请求
       this.currentNode = data
-      // 把 name 赋值给 this.form.nodeName 用于外显
       this.form.nodeName = data.name
-      // 点击保存的时候，判断后调用 handleNodeUpdate 方法
-      this.moduleDialogState.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.dialogStatus = 'update'
     },
     remove(data) {
       deleteNode(data.id).then(response => {
@@ -117,7 +121,7 @@ export default {
           duration: 2000
         })
         // 刷新树列表
-        this.queryModuleList()
+        this.queryModuleList(data.projectId)
       })
     },
     // 点击模块树的节点，查询对应模块id下的 api 列表
@@ -127,6 +131,45 @@ export default {
           // 把接口返回的数据，传到列表组件里
           this.$bus.$emit('getModuleApiList', response.data, data)
         })
+    },
+    // 刷新模块树
+    queryModuleList(projectId) {
+      getModuleList(projectId).then(response => {
+        this.data = response.data
+      })
+    },
+    handleNodeAdd() {
+      this.currentNode.name = this.form.nodeName
+      addModuleNode(this.currentNode).then(response => {
+        this.$message({
+          message: '新增成功',
+          type: 'success',
+          duration: 2000
+        })
+        this.dialogFormVisible = false
+        // 清空表单
+        this.form.nodeName = ''
+        // 把 projectId 传给父组件，刷新树列表
+        console.log('this.currentNode.projectId', this.currentNode.projectId)
+        this.queryModuleList(this.currentNode.projectId)
+      })
+    },
+    handleNodeUpdate() {
+      renameModuleNode({
+        id: this.currentNode.id,
+        name: this.form.nodeName
+      }).then(response => {
+        this.$message({
+          message: '更新成功',
+          type: 'success',
+          duration: 2000
+        })
+        this.dialogFormVisible = false
+        // 清空表单
+        this.form.nodeName = ''
+        // 刷新树列表
+        this.queryModuleList(this.currentNode.projectId)
+      })
     }
   }
 }
